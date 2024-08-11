554b329e5604010a8b579bfd8b77da9cb4ee5c07
kunalarya873 <kunalarya873@gmail.com>
2024-08-11 19:49:39 +0530

    Requirement file

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -131,7 +131,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Your SMTP host
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ""
+EMAIL_HOST_USER = "kunalarya88779@gmail.com"
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_SSL = False
 
